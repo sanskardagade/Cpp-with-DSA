5e15e609e5e50bcd11a03dbaa56b1d2536ba2bac
sanskardagade <sanskardagade07@gmail.com>
2026-01-22 17:23:55 +0530

    queue start

---
+++ L100 QUEUE/queue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+
+    // create a queue
+    queue<int>q;
+    q.push(11);
+    q.push(15);
+    q.push(13);
+    cout<<"Size of queue : "<<q.size()<<endl;
+    q.pop();
+    cout<<"Size of queue : "<<q.size()<<endl;
+
+    if(q.empty())cout<<"Queue is empty"<<endl;
+    else cout<<"Queue is not empty"<<endl;
+
+    cout<<"The front element of queue is "<<q.front()<<endl;
+
+
+
+}
