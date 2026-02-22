8357f0152a9a85069b2b67788614ad6a9b076bf2
sanskardagade <sanskardagade07@gmail.com>
2026-02-22 23:08:48 +0530

    section 3  done

---
+++ REVISION/Section-4/sortarray.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[] = {5,2,9,1,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+}
