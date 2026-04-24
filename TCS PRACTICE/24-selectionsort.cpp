861e7e11cbd94c0783e070d35a005543c0679de5
sanskardagade <sanskardagade07@gmail.com>
2026-04-25 03:24:55 +0530

    new

--- TCS PRACTICE/24-selectionsort.cpp
+++ TCS PRACTICE/24-selectionsort.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void BubbleSort(vector<int>& arr){
+void selectionSort(vector<int>& arr){
     int n = arr.size();
     for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+        int minIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j;
             }
         }
+        swap(arr[i],arr[minIdx]);
     }
 }
-
 
 int main(){
     int n;
@@ -20,7 +21,7 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    BubbleSort(arr);
+    selectionSort(arr);
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
