9f1ae91b27e1c836853fa7f9655d23a7a04c381f
sanskardagade <sanskardagade07@gmail.com>
2026-04-26 01:08:07 +0530

    last grinf

--- TCS PRACTICE/40-replace01.cpp
+++ TCS PRACTICE/40-replace01.cpp
@@ -1,17 +1,34 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     string s;
+//     cin >> s;
+
+//     for(int i = 0; i < s.length(); i++){
+//         if(s[i] == '0'){
+//             s[i] = '1';
+//         }
+//     }
+//     cout << s;
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string s;
-    cin >> s;
-
-    for(int i = 0; i < s.length(); i++){
-        if(s[i] == '0'){
-            s[i] = '1';
-        }
+int replacezero(int n){
+    if(n==0)return 0;
+    int result = 0;
+    int place = 1;
+    while(n>0){
+        int digit = n % 10;
+        if(digit == 0)digit = 1;
+        result += digit * place;
+        place *= 10;
+        n /= 10;
     }
-
-    cout << s;
-
-    return 0;
+    return result;
 }
+
