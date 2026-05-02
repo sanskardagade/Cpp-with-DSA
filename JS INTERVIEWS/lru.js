99dc1d1ea8013dcaa0693ba679b16b5cfd980f28
sanskardagade <sanskardagade07@gmail.com>
2026-05-02 14:09:23 +0530

    new learning

--- JS INTERVIEWS/lru.js
+++ JS INTERVIEWS/lru.js
@@ -1,11 +1,14 @@
 class LRUCache{
-    constructor(){
-
+    constructor(capacity){
+        this.capacity = capacity
+        this.map = new Map();
+        this.head = null
+        this.tail = null
     }
     get(key){
 
     }
     put(key,value){
-        
+
     }
 }
