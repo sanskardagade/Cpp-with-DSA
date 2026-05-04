a9be6f94272b700f7aa39abc17778985718db8d5
sanskardagade <sanskardagade07@gmail.com>
2026-05-04 15:26:47 +0530

    task scheduler done

---
+++ JS INTERVIEWS/taskscheduler.js
@@ -0,0 +1,105 @@
+class TaskScheduler{
+    constructor(concurrency){
+        this.concurrency = concurrency
+        this.runningTasks = 0;
+        this.__waitingQueue = []
+    }
+
+    getNextTask(){
+        if(this.runningTasks<this.concurrency && this.__waitingQueue.length>0){
+            const nextTasks = this.__waitingQueue.shift();
+            nextTasks();
+        }
+    }
+
+    addTask(task){
+        return new Promise((resolve, reject) => {
+            async function __taskRunner() {
+                this.runningTasks += 1;
+                try{
+                    const result = await task();
+                    console.log(`Result`,result)
+                    resolve(result)
+                }
+                catch(err){
+                    console.log(`Task Failed`,err);
+                    reject(err);
+                }
+                finally{
+                    this.runningTasks -= 1;
+                    //see if there is any task in queue,, if so run that 
+                    this.getNextTask();
+                }
+            }
+            if(this.runningTasks<this.concurrency){
+                __taskRunner.call(this);
+            }
+            else{
+                this.__waitingQueue.push(__taskRunner.bind(this));
+            }
+        })
+    }
+}
+
+
+// const scheduler = new TaskScheduler(2);
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 1'),1000))
+// )
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 2'),500))
+// )
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 3'),300))
+// )
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 4'),400))
+// )
+
+
+
+
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 1'),5*1000))
+// )
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 2'),5*1000))
+// )
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 3'),5*1000))
+// )
+
+// scheduler.addTask(
+//     () => new Promise((res)=>setTimeout(()=>res('Task 4'),5*1000))
+// )
+
+
+
+const scheduler = new TaskScheduler(10);
+
+function saveToDB(message){
+    return new Promise(
+        (res,rej) =>
+            setTimeout(() => {
+                console.log(`Message ${message} saved to DB!!`)
+                res();
+            }),
+            2*1000
+    )
+}
+
+function chat(){
+    const messages = Array(100).fill(null)
+
+    messages.forEach((_,index) => {
+        const message = `Message:${index}`
+        scheduler.addTask(() => saveToDB(message))
+    })
+}
