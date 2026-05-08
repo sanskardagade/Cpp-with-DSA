11b9b645832ce73c7375b700b9c2a8efa676db71
sanskardagade <sanskardagade07@gmail.com>
2026-05-08 23:49:05 +0530

    change

---
+++ INFOSYS/FoodStamps.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maxTastePoints(int n, int m, vector<int>& v, vector<int>& d) {
+
+        priority_queue<pair<long long,int>> pq;
+
+        // push initial values
+        for(int i = 0; i < n; i++) {
+            pq.push({v[i], i});
+        }
+
+        long long ans = 0;
+
+        while(m-- && !pq.empty()) {
+
+            auto top = pq.top();
+            pq.pop();
+
+            long long taste = top.first;
+            int idx = top.second;
+
+            // add current best taste
+            ans += taste;
+
+            // next value after buying again
+            long long nextTaste = taste - d[idx];
+
+            // push again if still positive
+            if(nextTaste > 0) {
+                pq.push({nextTaste, idx});
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    int n, m;
+
+    // Input number of dishes and number of purchases
+    // cout << "Enter n and m: ";
+    cin >> n >> m;
+
+    vector<int> v(n), d(n);
+
+    // Input initial taste values
+    // cout << "Enter taste values: ";
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    // Input decrease values
+    // cout << "Enter decrease values: ";
+    for(int i = 0; i < n; i++) {
+        cin >> d[i];
+    }
+
+    Solution obj;
+
+    long long result = obj.maxTastePoints(n, m, v, d);
+
+    // cout << "Maximum Taste Points: " << result << endl;
+    cout << result << endl;
+
+    return 0;
+}
---
+++ INFOSYS/maximumMSS.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int maximumMSS(vector<int>& a, int k) {
+
+        int n = a.size();
+        int ans = INT_MIN;
+
+        // try every subarray
+        for(int l = 0; l < n; l++) {
+
+            for(int r = l; r < n; r++) {
+
+                vector<int> inside;
+                vector<int> outside;
+
+                int sum = 0;
+
+                // split elements
+                for(int i = 0; i < n; i++) {
+
+                    if(i >= l && i <= r) {
+                        inside.push_back(a[i]);
+                        sum += a[i];
+                    }
+                    else {
+                        outside.push_back(a[i]);
+                    }
+                }
+
+                // smallest inside first
+                sort(inside.begin(), inside.end());
+
+                // biggest outside first
+                sort(outside.rbegin(), outside.rend());
+
+                int curr = sum;
+
+                // perform useful swaps
+                for(int t = 0; t < k; t++) {
+
+                    if(t >= inside.size() || t >= outside.size())
+                        break;
+
+                    // profitable swap
+                    if(outside[t] > inside[t]) {
+
+                        curr -= inside[t];
+                        curr += outside[t];
+                    }
+                }
+
+                ans = max(ans, curr);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    int n, k;
+
+    // cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> a(n);
+
+    // cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // cout << "Enter k: ";
+    cin >> k;
+
+    Solution obj;
+
+    int result = obj.maximumMSS(a, k);
+
+    cout << result << endl;
+
+}
